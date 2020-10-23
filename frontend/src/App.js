89151817637d877c6a6a201eb8aff10ebfa016f6
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-10-23 10:09:51 -0700

    Added redux set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,24 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Landing from './components/LandingPage/Landing';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch></Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch></Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
