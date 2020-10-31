4ee38dd8b28faf89a6c6a10c01ce0a1132d7cf87
Shravanthi <57304916+shravanthi94@users.noreply.github.com>
2020-10-31 08:56:19 -0700

    Added customer message routes in backend

---
+++ backend/routers/customer/message.js
@@ -0,0 +1,79 @@
+const express = require('express');
+
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const { checkAuth } = require('../../middleware/auth');
+
+const Message = require('../../models/MessageModel');
+
+// @route  POST yelp/customer/message
+// @desc   Customer reply message route
+// @access Private
+router.post(
+  '/:id',
+  [checkAuth, [check('text', 'Message text is required').notEmpty()]],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { text } = req.body;
+      //   const customerId = req.user.id;
+      const messageId = req.params.id;
+
+      const conversation = await Message.findById(messageId);
+      conversation.messages.unshift({
+        usertype: 'customer',
+        text,
+      });
+
+      await conversation.save();
+
+      res.status(200).send('Successfully sent the message');
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Server Error');
+    }
+  },
+);
+
+// @route  GET yelp/customer/message/all
+// @desc   Get all customer messages
+// @access Private
+router.get('/all', checkAuth, async (req, res) => {
+  const customerId = req.user.id;
+  try {
+    const conversations = await Message.find({ customer: customerId });
+    if (!conversations) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'Conversations not found' }] });
+    }
+    res.status(200).json(conversations);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  GET yelp/customer/message/all
+// @desc   View each message conversation with a restaurant
+// @access Private
+router.get('/view/:id', checkAuth, async (req, res) => {
+  const messageId = req.params.id;
+  try {
+    const conversation = await Message.findById(messageId);
+    if (!conversation) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'Conversation not found' }] });
+    }
+    res.status(200).json(conversation);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
--- backend/routers/restaurant/message.js
+++ backend/routers/restaurant/message.js
@@ -26,54 +26,63 @@ router.post(
       }
       const { text, customerId } = req.body;
       const restaurantId = req.user.id;
-      const messages = [];
-      const msgObj = {
-        usertype: 'restaurant',
-        text,
-      };
-      messages.push(msgObj);
-      const newMessage = new Message({
+
+      const conversation = await Message.find({
         restaurant: restaurantId,
         customer: customerId,
-        messages,
       });
-      await newMessage.save();
 
-      res.status(200).send('Successfully sent the message');
-    } catch (err) {
-      console.log(err);
-      res.status(500).send('Server Error');
-    }
-  },
-);
+      if (!conversation) {
+        const messages = [];
+        const msgObj = {
+          usertype: 'restaurant',
+          text,
+        };
+        messages.push(msgObj);
+        const newMessage = new Message({
+          restaurant: restaurantId,
+          customer: customerId,
+          messages,
+        });
+        await newMessage.save();
 
-// @route  POST yelp/restaurant/message/:id
-// @desc   Restaurant reply message route
-// @access Private
-router.post(
-  '/:id',
-  [checkAuth, [check('text', 'Message text is required').notEmpty()]],
-  async (req, res) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        res.status(200).send('Successfully sent the message');
+      } else {
+        conversation.messages.unshift({
+          usertype: 'restaurant',
+          text,
+        });
+
+        await conversation.save();
+
+        res.status(200).send('Successfully sent the message');
       }
-      const { text } = req.body;
-      const messageId = req.params.id;
-
-      const message = await Message.findById(messageId);
-      message.messages.unshift({
-        usertype: 'restaurant',
-        text,
-      });
-      await message.save();
-      res.status(200).send('Successfully sent the message');
     } catch (err) {
       console.log(err);
       res.status(500).send('Server Error');
     }
   },
 );
+
+// @route  GET yelp/restaurant/message/:customerId
+// @desc   View conversations between customer & restaurant
+// @access Private
+router.get('/:customerId', async (req, res) => {
+  const restaurantId = req.user.id;
+  const { customerId } = req.params;
+  try {
+    const messages = await Message.find({
+      restaurant: restaurantId,
+      customer: customerId,
+    });
+    if (!messages) {
+      return res.status(400).json({ errors: [{ msg: 'No messages found' }] });
+    }
+    res.status(200).json(messages);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
